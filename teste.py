e8382690efbdd94f415550f52c9b8161e3c2aed5
saxmanrs <soares001>
2018-07-19 15:03:54 -0300

    Cadastrar pessoas

--- teste.py
+++ teste.py
@@ -1,3 +1,26 @@
-convites =  {'simples': 30 ,'normal': 60, 'caro': 100,'carissimo':200}
+def listar(nomes):
+    print'Listar os nomes: '
+    for nome in nomes:
+        print nome
 
-print(convites['simples'])
+
+
+
+def cadastrar(nomes):
+    print'Digite o seu nome: '
+    nomes = raw_input()
+    nomes.append(nomes)
+
+
+
+def menu ():
+    nomes = []
+    escolha = ''
+    while(escolha != '0' ):
+        print('Digite a opção desejada: 1 - cadastrar 2 - Listar 0 - Sair')
+        escolha = raw_input()
+        if (escolha == '1'):
+            cadastrar()
+        if (escolha == '2'):
+            listar()
+        menu()
